d725ea59a6a6905e413eea96140650e074adf409
Vinh Huynh <vinhhuynh115@gmail.com>
2017-10-20 12:28:08 +0700

    Add project

---
+++ Assets/Crossroads.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crossroads : MonoBehaviour {
+
+	public enum CRPosition
+	{
+		CR_LEFT_TOP,
+		CR_MID_TOP,
+		CR_RIGHT_TOP,
+		CR_LEFT_MIDDLE,
+		CR_MID_MIDDLE,
+		CR_RIGHT_MIDDLE,
+		CR_LEFT_BOTTOM,
+		CR_MID_BOTTOM,
+		CR_RIGHT_BOTTOM
+	}
+
+	public CRPosition position;
+	public List<StreetController> streetList;
+
+	public StreetController changeStreet(StreetController curStreet){
+		for (int i = 0; i < streetList.Count; i++) {
+			if (streetList [i] != curStreet)
+				return streetList [i];
+		}
+		return streetList [0];
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Model/Street/StreetRange.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class StreetRange {
+	public float pointNorth { get; set; }
+	public float pointSouth { get; set; }
+	public float pointWest { get; set; }
+	public float pointEast { get; set; }
+}
